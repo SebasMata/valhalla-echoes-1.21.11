2b73f8a297eeceda6073e16a5f538cfee5af26e7
Sebas Mata <sebasmata09@gmail.com>
2026-02-12 18:13:44 -0600

    Add initial implementation of suspicious substance item and localization

--- src/main/java/com/mataflex/ModItems.java
+++ src/main/java/com/mataflex/ModItems.java
@@ -1,19 +1,41 @@
 package com.mataflex;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ToolMaterial;
+import net.minecraft.world.level.Level;
 
 import java.util.function.Function;
 
 public class ModItems {
 
     public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new, new Item.Properties());
+
+    public static final ToolMaterial MYSTICAL_TOOL_MATERIAL = new ToolMaterial(
+            BlockTags.INCORRECT_FOR_WOODEN_TOOL,
+            455,
+            5.0F,
+            1.5F,
+            15,
+            ToolMaterial.DIAMOND.repairItems()
+    );
+
+    public static final Item MYSTICAL_SWORD = register(
+            "mystical_sword",
+            Item::new,
+            new Item.Properties().sword(MYSTICAL_TOOL_MATERIAL, 18f, 6f)
+    );
 
     public static <GenericItem extends Item> GenericItem register(String name, Function<Item.Properties, GenericItem> itemFactory, Item.Properties settings) {
         // Create the item key.
@@ -28,11 +50,15 @@ public class ModItems {
         return item;
     }
 
+
+
     public static void initialize() {
         // Get the event for modifying entries in the ingredients group.
         // And register an event handler that adds our suspicious item to the ingredients group.
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.INGREDIENTS)
                 .register((itemGroup) -> itemGroup.accept(ModItems.SUSPICIOUS_SUBSTANCE));
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT)
+                .register((itemGroup) -> itemGroup.accept(ModItems.MYSTICAL_SWORD));
 
     }
 
